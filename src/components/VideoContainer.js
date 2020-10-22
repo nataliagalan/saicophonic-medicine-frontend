a30eb3ab97f3090192d4b35f46a7c322787af687
Natalia Galán <hi@nataliagalan.me>
2020-10-22 00:16:27 -0500

    dynamic rendering of lyrics card based on video.songs

--- src/components/VideoContainer.js
+++ src/components/VideoContainer.js
@@ -7,6 +7,7 @@ import Row from 'react-bootstrap/Row';
 import VideoCard from './VideoCard';
 import LyricsCard from './LyricsCard';
 import Line from './Line';
+import SongContainer from './SongContainer';
 
 
 class VideoContainer extends Component {
@@ -14,6 +15,7 @@ class VideoContainer extends Component {
   //this.props.video.songs[0].timestamp - value of first stamp by default
 
   render() {
+    
     // console.log(this.props.video, "inside video container");
     return (
       <>
@@ -22,7 +24,7 @@ class VideoContainer extends Component {
             <VideoCard {...this.props.video}/>
           </Col>
           <Col sm={4}>
-            <LyricsCard {...this.props.video}/>
+            <SongContainer {...this.props.video} />
           </Col>
         </Row>
         <Line color="black" />
