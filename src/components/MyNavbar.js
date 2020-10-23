15f2dd94f04ce0c1ce8a102ef54f7b3b2cec1c70
Natalia <n8galan@gmail.com>
2020-10-23 11:09:15 -0500

    added logo and gradient to navbar

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import {Link} from 'react-router-dom';
+import logo from '../logo.png'
 
 const MyNavbar = () => {
 
     return (
       <>
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Link to="/videos">Logo</Link>
+      <Navbar collapseOnSelect expand="lg" className="nav-bg" sticky="top">
+        {/* <Link to="/videos">Logo</Link> */}
+        <Navbar>
+        <Navbar.Brand><img src={logo} style={{maxWidth: '80px'}} /></Navbar.Brand>
+        </Navbar>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
