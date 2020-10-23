5346d0fe69c1d97de013b6c2838f6a4c7a8de70c
Natalia Galán <hi@nataliagalan.me>
2020-10-23 12:22:03 -0500

    added header and more styling

--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -8,16 +8,18 @@ const MyNavbar = () => {
 
     return (
       <>
-      <Navbar collapseOnSelect expand="lg" className="nav-bg" sticky="top">
+      <Navbar collapseOnSelect expand="lg" className="nav-bg" fixed="top" >
         {/* <Link to="/videos">Logo</Link> */}
-        <Navbar>
+        <Navbar style={{cursor: 'pointer'}}>
+        <Link to="/videos">
         <Navbar.Brand><img src={logo} style={{maxWidth: '80px'}} /></Navbar.Brand>
+        </Link>
         </Navbar>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="nav-toggle" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ml-auto">
             {/* <Nav.Link href="/videos/new" id="menu-toggle">ADD VIDEO +</Nav.Link> */}
-            <Link to="/videos/new" id="menu-toggle">ADD VIDEO +</Link>
+            <Link to="/videos/new" id="menu-toggle" className="nav-font-style">Add Video +</Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
