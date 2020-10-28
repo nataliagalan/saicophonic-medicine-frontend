97aea7ec18eb9c071635f0802e509ddf9dcede9a
Natalia <n8galan@gmail.com>
2020-10-28 00:48:54 -0500

    implemented full  authorization including Edit page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,16 +2,50 @@ import React from 'react'
 import { connect  } from 'react-redux';
 import { loginSuccess } from '../actions/auth'
 import { logoutSuccess } from '../actions/auth'
+import { currentUser } from '../actions/auth'
 import {Link} from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 
 class Login extends React.Component {
+
+  componentDidMount() {
+    const token = localStorage.getItem('myAppToken') 
+    if(!token){
+      this.props.history.push('/admin')
+    } else {
+      this.fetchData()
+    }
+  }
+
   state = {
-    username: 'ursula',
-    password: '1', 
+    username: 'ursula2',
+    password: '2', 
     error: null
+  }
+
+  fetchData = async () => {
+    const token = localStorage.getItem('myAppToken') 
+    const reqObj = {
+      method: 'GET',
+      headers: {
+      'Authorization': `Bearer ${token}`
+      }
+    }
+    const res = await fetch('http://localhost:3001/api/v1/current_user', reqObj);
+    const data = await res.json();
+    if(data.error) {
+      this.props.history.push('/admin')
+    } else {
+      //need to store the user (data) in store state
+      this.props.currentUser(data)
+    }
+  }
+
+  handleLogout = () => {
+    localStorage.removeItem('myAppToken')
+    this.props.logoutSuccess()
   }
 
   handleChange = (e) => {
@@ -40,7 +74,7 @@ class Login extends React.Component {
       // console.log(data, "------------");
       localStorage.setItem('myAppToken', data.token)
       this.props.loginSuccess(data)
-      this.props.history.push('/videos/new')
+      this.props.history.push('/videos')
 
     }
 
@@ -51,38 +85,65 @@ class Login extends React.Component {
   return (
     <div className="login-page-content-wrapper">
     <Form onSubmit={this.handleSubmit}>
-    { this.state.error && <h4 style={{ color: 'red'}}>{this.state.error}</h4> }
-      <Form.Row>
-        <Col>
-          <Form.Control 
-            name="username"
-            label="username" 
-            onChange={this.handleChange}
-            value={this.state.username}
-            // placeholder="username" 
-            />
-        </Col>
-        <Col>
-          <Form.Control
-            name="password"
-            label="password" 
-            type="password" 
-            onChange={this.handleChange} 
-            value={this.state.password}
-            // placeholder="pw" 
-            />
-        </Col>
+      { this.state.error && <h4 style={{ color: 'red'}}>{this.state.error}</h4> }
+      {
+        (!this.props.auth.id) ?
+        (<Form.Row>
+          <Col>
+            <Form.Control 
+              name="username"
+              label="username" 
+              onChange={this.handleChange}
+              value={this.state.username}
+              // placeholder="username" 
+              />
+          </Col>
+          <Col>
+            <Form.Control
+              name="password"
+              label="password" 
+              type="password" 
+              onChange={this.handleChange} 
+              value={this.state.password}
+              // placeholder="pw" 
+              />
+          </Col>
+          <Button 
+            variant="primary" 
+            type="submit">
+            Login
+          </Button>
+        </Form.Row>)
+        :
+        (<>
         <Button 
+          as={Link}
+          to='/admin'
+          onClick={ this.handleLogout }
+          variant="primary">
+        Logout
+        </Button>
+
+        <Button 
+          as={Link}
+          to='/videos/new'
           variant="primary" 
           type="submit">
-          Login
+        Add Video
         </Button>
-      </Form.Row>
+      </>)
+ 
+      }
+
     </Form>
     </div>
     );
   }
 }
+
+
+
+
 
 //what gets returned here becomes available as props within this component
 const mapStateToProps = (state) => {
@@ -92,6 +153,7 @@ const mapStateToProps = (state) => {
 }
 
 const mapDispatchToProps = {
+  currentUser,
   loginSuccess,
   logoutSuccess
 }
