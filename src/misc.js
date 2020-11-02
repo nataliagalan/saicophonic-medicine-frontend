fa8887cc54ebc96a68af601fdfbaa5388ce3033f
Natalia <n8galan@gmail.com>
2020-11-02 00:09:01 -0600

    progress on implementing typeahead, dropdown working and links to show page correctly

--- src/misc.js
+++ src/misc.js
@@ -1,24 +1,24 @@
 
 
-      <AsyncTypeahead
-      {...this.state}
-      // isLoading={this.state.isLoading}
-      id="async-pagination-example"
-      labelKey={option => `${option.id}`}
-      // labelKey="login"
-      // maxResults={PER_PAGE - 1}
-      minLength={2}
-      onInputChange={this._handleInputChange}
-      // onPaginate={this._handlePagination}
-      onSearch={this._handleSearch}
-      paginate
-      placeholder="Search by Artist/Band, Song or Lyrics..."
-      options={this.state.options}
-      renderMenuItemChildren={option => (
-        <div key={option.id}>
-          {option.band}
-        </div>
-      )}
+    //   <AsyncTypeahead
+    //   {...this.state}
+    //   // isLoading={this.state.isLoading}
+    //   id="async-pagination-example"
+    //   labelKey={option => `${option.id}`}
+    //   // labelKey="login"
+    //   // maxResults={PER_PAGE - 1}
+    //   minLength={2}
+    //   onInputChange={this._handleInputChange}
+    //   // onPaginate={this._handlePagination}
+    //   onSearch={this._handleSearch}
+    //   paginate
+    //   placeholder="Search by Artist/Band, Song or Lyrics..."
+    //   options={this.state.options}
+    //   renderMenuItemChildren={option => (
+    //     <div key={option.id}>
+    //       {option.band}
+    //     </div>
+    //   )}
    
-      // useCache={false}
-    /> 
+    //   // useCache={false}
+    // /> 
