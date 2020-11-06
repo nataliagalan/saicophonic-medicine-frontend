17c7ec7186f0a1b47fe031b57af5662678e4269a
Natalia Galán <hi@nataliagalan.me>
2020-11-06 00:40:59 -0600

    set reducer and action to toggle grid!

---
+++ src/actions/toggleGrid.js
@@ -0,0 +1,6 @@
+export const toggleGrid = (payload) => {
+  return {
+    type: "TOGGLE_GRID",
+    payload
+  };
+};
---
+++ src/reducers/toggleGrid.js
@@ -0,0 +1,13 @@
+
+
+
+const toggleGrid = (state=false, action) => {
+  switch(action.type){
+      case 'TOGGLE_GRID':
+          return !state
+      default:
+          return state
+  }
+}
+
+export default toggleGrid
