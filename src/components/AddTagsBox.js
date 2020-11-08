664f6f61c862c1c9f38e5e75788a66f9a0d9892b
Natalia Galán <hi@nataliagalan.me>
2020-11-08 17:11:59 -0600

    tag functionality finally worksss!

--- src/components/AddTagsBox.js
+++ src/components/AddTagsBox.js
@@ -1,31 +1,57 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector  } from 'react-redux';
 import '../Tags.css';
 
 import { tags } from '../actions/tags'
+import { removeTagsAction } from '../actions/tags'
 import { getVideo } from '../actions/video'
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import { XIcon } from '@primer/octicons-react'
 
 const AddTagsBox = (props) => {
 
   // useSelector is similar to setStateToProps
   const video = useSelector(state => state.video);
-  const updatedTags = useSelector(state => state.tags);
+  let currentTags = useSelector(state => state.tags);
 
   //useDispatch is similar to setDispatchToProps
   const dispatch = useDispatch();
-
+  const [currentVideo, setCurrentVideo] = useState(video);
 
   const handleAddTag = (e) => {
     let newTag = e.target.value.trim()
-    if (e.key === "Enter" && newTag !== "") {
+    if (newTag !== "") {
     dispatch(tags(newTag));
     video.tags = [...video.tags, {name: newTag}]
     dispatch(getVideo(video));
     e.target.value = ""
     }
+  }
+
+  const removeTag = (idxToRemove) => {
+
+    console.log(idxToRemove, "idxtoremove");
+    // optimistic tags do not have id yet
+    
+    let filteredTags = video.tags.filter((_, index) => index !== idxToRemove);
+    console.log(filteredTags, "filteredTags");
+    
+    dispatch(getVideo(video));
+    
+    setCurrentVideo(video.tags = filteredTags)
+    // video.tags = video.tags.filter((tag) => tag.id !== idToRemove);
+
+    // dispatch(getVideo(video));
+    // dispatch(removeTagsAction(tagNameToRemove));
+
+
+    // let tagToRemove = video.tags.find((tag) => tag.id === idToRemove);
+    // console.log(tagToRemove, "====tagtoremove===");
+
+    // dispatch(deleteVideo(updatedVideos));
+
   }
 
   return (
@@ -36,7 +62,11 @@ const AddTagsBox = (props) => {
           video.tags ?
           video.tags.map((tag, idx) => <li className="tag" key={idx}>
           <span>{tag.name}</span>
-          <XIcon size={16} />
+          <Button onClick={() => removeTag(idx)}>
+            <XIcon 
+              key={idx}
+              size={16} />
+          </Button>
           </li>)
           :
           null
@@ -44,12 +74,11 @@ const AddTagsBox = (props) => {
         
       </ul>
       <Form.Group controlId="tags-input">
-        <Form.Control 
+        <Form
         placeholder="Press enter to add tags"
         as="textarea" 
         rows={1}
-        // onKeyUp={(e) => (e.key === "Enter" ? handleAddTag(e) : null)} 
-        onKeyUp={handleAddTag} 
+        onKeyUp={(e) => (e.key === "Enter" ? handleAddTag(e) : null)} 
        />
       </Form.Group>
     </div>
--- src/reducers/tags.js
+++ src/reducers/tags.js
@@ -7,9 +7,32 @@ const tags = (state = initialValue, action) => {
   switch (action.type) {
     case "ADD_TAG":
       return [...state, action.newTag ]
+    case "REMOVE_TAG":
+      //filter through tag array and remove action.tagNameToRemove
+      let filteredTags = state.filter(tag => tag !== action.tagNameToRemove)
+      
+      return filteredTags
+
+      
     default:
       return state;
   }
 }
 
 export default tags
+
+
+
+
+// const initialValue = []
+
+// const tags = (state = initialValue, action) => {
+//   switch (action.type) {
+//     case "ADD_TAG":
+//       return [...state, action.newTag ]
+//     default:
+//       return state;
+//   }
+// }
+
+// export default tags
