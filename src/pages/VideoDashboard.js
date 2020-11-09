a4b5a83f1db53819733a9f1e452926fb5fbb64ed
Natalia Galán <hi@nataliagalan.me>
2020-11-09 13:44:14 -0600

    fetch request via usePaginatedQuery now working!

--- src/pages/VideoDashboard.js
+++ src/pages/VideoDashboard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { usePaginatedQuery } from 'react-query';
 import { useDispatch, useSelector  } from 'react-redux';
 import { getVideos } from '../actions/videos';
 import { currentUser } from '../actions/auth';
@@ -6,37 +7,52 @@ import { setFilter } from '../actions/setFilter';
 import { toggleTabs } from '../actions/toggleTabs';
 
 import Container from 'react-bootstrap/Container';
+import Pagination from 'react-bootstrap/Pagination';
 import Nav from 'react-bootstrap/Nav';
 import VideoContainer from '../components/VideoContainer';
 
 
 const VideoDashboard = (props) => {
+  
+  //useSelector is similar to setStateToProps
+  const videos = useSelector(state => state.videos);
+  const auth = useSelector(state => state.auth);
+  const filteredByAll = useSelector(state => state.filteredByAll);
+  const filteredByBand = useSelector(state => state.filteredByBand);
+  const filteredBySong = useSelector(state => state.filteredBySong);
+  const filteredByLyrics = useSelector(state => state.filteredByLyrics);
+  const filter = useSelector(state => state.setFilter);
+  const showTabs = useSelector(state => state.toggleTabs.showTabs);
+  
+  //useDispatch is similar to setDispatchToProps
+  const dispatch = useDispatch();
+  
+  const fetchVideos = async (key, page) => {
+    const res = await fetch(`http://localhost:3001/api/v1/videos?page=${page}`);
+    // const res = await fetch(`http://localhost:3001/api/v1/videos`);
+    const videos = await res.json();
+    dispatch(getVideos(videos));
+    
+    // return res.json();
+    
+    return videos
+  };
 
-   //useSelector is similar to setStateToProps
-   const videos = useSelector(state => state.videos);
-   const auth = useSelector(state => state.auth);
+  const [ page, setPage ] = useState(1);
+  
+  const { resolvedData, latestData, status } = usePaginatedQuery(['videos', page], fetchVideos);
+  // console.log(resolvedData, "==resolvedData under usePaginatedQuery==");
 
-   const filteredByAll = useSelector(state => state.filteredByAll);
-   const filteredByBand = useSelector(state => state.filteredByBand);
-   const filteredBySong = useSelector(state => state.filteredBySong);
-   const filteredByLyrics = useSelector(state => state.filteredByLyrics);
-   const filter = useSelector(state => state.setFilter);
-   const showTabs = useSelector(state => state.toggleTabs.showTabs);
-
-   //useDispatch is similar to setDispatchToProps
-   const dispatch = useDispatch();
-
+//componentdidupdate
+  
   useEffect(() => {
+
+    //both resolvedData and latestDate are always undefined here
+    // console.log(resolvedData, "==resolveddata in useeffect====");
+    // console.log(latestData, "==latestdata in useffect====");
+
     // code to run on component mount
     const token = localStorage.getItem('myAppToken')
-
-    const fetchVideos = async () => {
-      const res = await fetch(`http://localhost:3001/api/v1/videos`);
-      // const res = await fetch(`http://localhost:3001/api/v1/videos?page=3`);
-      const videos = await res.json();
-      dispatch(getVideos(videos));
-
-    };
 
     const fetchUser = async () => {
       const token = localStorage.getItem('myAppToken') 
@@ -56,12 +72,15 @@ const VideoDashboard = (props) => {
       }
     }
 
-    if(!token){
-      fetchVideos()
-    } else {
+    // if(!token){
+    //   fetchVideos()
+    // } else {
+    //   fetchUser()
+    //   fetchVideos()
+    // }
+    if(token){
       fetchUser()
-      fetchVideos()
-    }
+    } 
   }, [])
 
   const displayFilterTabs = () => {
@@ -98,9 +117,18 @@ const VideoDashboard = (props) => {
   }
 
   const findVideos = () => {
+    console.log(resolvedData, "==resolvedData under findVideos==");
+    console.log(latestData, "==latestData under findVideos==");
+    
     switch (filter) {
       case "none":
-        return videos
+        //here return resolvedData if status === 'success'
+        if(status === 'success'){
+        return resolvedData } else {
+          return videos
+        }
+    
+        // return videos
       case "all":
         return filteredByAll
       case "bands":
@@ -119,8 +147,11 @@ const VideoDashboard = (props) => {
   }
 
 
+  
   return ( 
     <Container fluid>
+      {/* {test()} */}
+      {/* {status === 'success' ? dispatch(getVideos(resolvedData)) : null} */}
     <div className="page-content-wrapper">
       <div className="dashboard-header">
         <div className="dashboard-header-title"><h1 className="header-text">Saicophonic Medicine</h1></div>
@@ -128,6 +159,14 @@ const VideoDashboard = (props) => {
       </div>
       {showTabs === "true" ? displayFilterTabs() : null}
       <VideoContainer videos={findVideos()} />
+      <Pagination>
+        <Pagination.Prev />
+        <Pagination.Item>{1}</Pagination.Item>
+        <Pagination.Item>{2}</Pagination.Item>
+        <Pagination.Item>{3}</Pagination.Item>
+        <Pagination.Next />
+      </Pagination>
+
     </div>
     </Container>
   );
