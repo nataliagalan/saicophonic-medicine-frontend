757e8eb067e1e160b3b2ca27dc9d81b0b580b622
Natalia <n8galan@gmail.com>
2020-11-09 11:24:44 -0600

    refactored VideoDashboard into functional component to implement react-query hooks

--- src/misc.js
+++ src/misc.js
@@ -1,413 +1,123 @@
 
 
 
-body {
-  margin: 0;
-  font-family: "Futura-Pt", -apple-system, BlinkMacSystemFont, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
-
-code {
-  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
-    monospace;
-}
-
-
-.dashboard-header {
-  padding-top: 8rem;
-  padding-bottom: 5rem;
-}
-
-.header-text {
-  font-family: pragmatapro-fraktur, -apple-system, BlinkMacSystemFont, "Helvetica Neue", Arial, "Noto Sans", serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-weight: 600;
-  font-size: 4.5rem;
-  color: #EBDFF7;
-  margin-left: 1rem;
-  margin-right: 1rem;
-}
-
-.nav-bg {
-  /* background-color: rgb(15, 15, 15); */
-  background: linear-gradient(to bottom, black 0%, rgba(0, 0, 0, 0.738) 19%, rgba(0, 0, 0, 0.541) 34%, rgba(0, 0, 0, 0.382) 47%, rgba(0, 0, 0, 0.278) 56.5%, rgba(0, 0, 0, 0.194) 65%, rgba(0, 0, 0, 0.126) 73%, rgba(0, 0, 0, 0.075) 80.2%, rgba(0, 0, 0, 0.042) 86.1%, rgba(0, 0, 0, 0.021) 91%, rgba(0, 0, 0, 0.008) 95.2%, rgba(0, 0, 0, 0.002) 98.2%, transparent 100%);
-  /* padding-right: 8rem;
-  padding-left: 8rem; */
-  
-}
-
-.nav-toggle {
-  border: none;
-}
-
-
-.nav-toggle,
-.btn:active,
-.btn:focus,
-.btn.active {
-  /* background-image: none; */
-  outline: 0;
-  -webkit-box-shadow: none;
-          box-shadow: none;
-   outline: none !important;
-}
-
-.nav-font-style {
-  font-family: pragmatapro-fraktur, -apple-system, BlinkMacSystemFont, "Helvetica Neue", Arial, "Noto Sans", serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  font-weight: 400;
-  font-size: 1.5rem;
-  color: #EBDFF7;
-  padding-left: 1.5rem;
-
-}
-
-@media screen and (max-width: 766px) {
-  .header-text { 
-    font-size: 3em; 
- }
-  .header-subttext { 
-    font-size: 1rem; 
- }
-
- /* .nav-bg {
-  padding-right: 1rem;
-  padding-left: 1.2rem;
-} */
- 
-}
-
-/* @media screen and (max-width: 320px) {
-  .nav-bg {
-    padding-right: 0rem;
-    padding-left: 0.2rem;
-   
-  }
-
-} */
-
-
-/* FIXES THE VIDEO IN PLACE */
-.video-wrapper {
-  position: -webkit-sticky;
-  position: sticky;
-  top: 8rem;
-}
-
-@media screen and (max-width: 766px) {
-
-  .video-wrapper {
-    position: fixed !important;
-    /* top: 3rem; */
-    top: 1vw;
-    width: 100vw;
-    padding-right: 11px;
-    padding-left: 11px;
-    margin-left: -50vw;
-    margin-top: 100px;
-    left: 50%;
-    z-index: 2;
-    
-  }
-
-  .edit-and-new-form {
-    margin-top: 70vw;;
-    z-index: 1;
-  }
-
-  .duration-seconds {
-    display: none;
-  }
-
-  /* ! adjusts form width */
-
-  .new-and-edit-video-page {
-    margin-left: 2rem !important;
-    margin-right: 2rem !important;
-  }
-
-}
-
-/* FIXES THE VIDEO IN PLACE */
-
-.header-subtext {
-  font-weight: 400;
-  font-size: 1.5rem;
-  color: #EBDFF7;
-  padding-top: 1.7rem;
-  letter-spacing: .09em;
-  margin-left: 1rem;
-  margin-right: 1rem;
-}
-
-.btn-primary {
-  background: none;
-  border: 1;
-  border-color: #ebdff7de;
-  font-size: 1.2rem;
-}
-
-.btn-primary:hover {
-  background: #ebdff7;
-  border-color: #ebdff7de;
-  color: #000;
-}
-
-.dynamic-input-btn {
-  font-size: 2rem;
-  border: none;
-}
-.dynamic-input-btn:hover {
-  color: rgb(250, 192, 0);
-  background: none;
-}
-
-.new-and-edit-video-page {
-  padding-top: 8rem;
-  margin-left: 4rem;
-  margin-right: 4rem;
-  text-align: center;
-}
-
-.duration-seconds, h3 {
-  color: #EBDFF7;
-  letter-spacing: .09em;
-}
-
-#formBasicRangeCustom::-webkit-slider-thumb {
-  background: rgb(250, 192, 0);
-  
-}
-
-.page-content-wrapper {
-  /* TODO ADJUST THIS SOLUTION LATER */
-  padding-top: 3.8rem;
-  margin-left: 2rem;
-  margin-right: 2rem;
-  text-align: center;
-}
-.song-accordion {
-  background: #fdfbff;
-  border-radius: 0;
-}
-
-.plus-accordion {
-  background: #fffced;
-  /* border-radius: 0; */
-}
-
-.custom-spacer {
-  padding-bottom: 1.8rem;
-}
-
-.login-page-content-wrapper {
-  padding-top: 13rem;
-  margin-left: 2rem;
-  margin-right: 2rem;
-}
-
-.accordion-time-title {
-  color: #000;
-}
-
-/* this adds scrolling ability to lyrics accordion */
-.overflow-auto {
-  max-height: 380px;
-  text-align: left;
-  padding-left: 3rem;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// CSSS
-
-
-
-
-
-
-
-return (
-  <>
-  <Navbar collapseOnSelect expand="lg" className="nav-bg" fixed="top" >
-
-
-    <Navbar >
-      <Link to="/videos">
-        <Navbar.Brand><img src={logo} className="imgFluid" style={{maxWidth: '50px'}} /> </Navbar.Brand>
-      </Link>
-    </Navbar>
-    <Navbar >
-      <SearchForm  />
-    </Navbar>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" className="nav-toggle" />
-
-    <Navbar.Collapse id="responsive-navbar-nav">
-      <Nav className="ml-auto">
-        {/* <Nav.Link href="/videos/new" id="menu-toggle">ADD VIDEO +</Nav.Link> */}
-        <Link to="/videos/new" id="menu-toggle" className="nav-font-style">Add Video</Link>
-      {
-        (auth.id) ?
-        (<Link 
-          to='/admin' 
-          id="menu-toggle" className="nav-font-style"
-          onClick={ handleLogout }>
-          Logout
-        </Link>)
-        : 
-        (<Link to='/admin' id="menu-toggle" className="nav-font-style" >
-          Login
-        </Link>)
-      }
-      </Nav>
-    </Navbar.Collapse>
-
-      
-  </Navbar>
-  </>
-);
-
-
-
-
-
-
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { getVideos } from '../actions/videos';
-import VideoCard from '../components/VideoCard';
-import VideoDashboard from './VideoDashboard'
+import { currentUser } from '../actions/auth';
+import { setFilter } from '../actions/setFilter';
+import { toggleTabs } from '../actions/toggleTabs';
 
-class SearchResults extends Component {
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import VideoContainer from '../components/VideoContainer';
 
-  // state = {
-  //   filteredVideos: [],
-  //   query: "",
-  //   allCount: 0,
-  //   bandCount: 0,
-  //   songCount: 0,
-  //   lyricsCount: 0
-  // }
 
-  //THIS IS THE PARENT OF VIDEO DASHBOARD
+class VideoDashboard extends Component {
 
   componentDidMount() {
-    // console.log(this.props.location.pathname, "=====SEARCH RESULTS PAGE====");
-    let query = this.props.location.pathname.split('/')[3]
-    this.setState({ query })
-    let path = this.props.location.pathname
-    // this.fetchVideos(path)
+    const token = localStorage.getItem('myAppToken') 
+    if(!token){
+      this.fetchVideos()
+    } else {
+      this.fetchUser()
+    }
+    this.fetchVideos()
   }
 
-  // define fetchvideos in app js
-  //info needed: query / path  and tab name 
-  // pass in the tab and the path as arguments when invoked
-  // how will the video dashboard know the pathname or query
+  fetchVideos = async () => {
+    const res = await fetch('http://localhost:3001/api/v1/videos');
+    const videos = await res.json();
+    this.props.getVideos(videos);
+  };
 
-  //invoke it from both the video dashboard and the searchform
+  fetchUser = async () => {
+    const token = localStorage.getItem('myAppToken') 
+    const reqObj = {
+      method: 'GET',
+      headers: {
+      'Authorization': `Bearer ${token}`
+      }
+    }
+    const res = await fetch('http://localhost:3001/api/v1/current_user', reqObj);
+    const data = await res.json();
+    if(data.error) {
+      this.props.history.push('/admin')
+    } else {
+      //need to store the user (data) in store state
+      this.props.currentUser(data)
+    }
+  }
 
-  // fetchVideos = async (tab) => {
-  // // fetchVideos = (tab) => {
-  //   // console.log(tab, "=====EVENT IN FETCH VIDEOS=====");
-
-  //   let path = this.props.location.pathname
-  //   const res = await fetch(`http://localhost:3001/api/v1/${path}`);
-  //   const videos = await res.json();
-
-
-  //   this.filterResults(videos, tab)
-  //   this.setLocalStateCount(videos)
-  //   // this.props.getVideos(videos);
-  // };
-
-  // setLocalStateCount = (videos) => {
-  //   let query = this.props.location.pathname.split('/')[3].toLowerCase()
-  //   let filteredBySong = videos.filter(function(video) {
-  //     return video.songs.some(function(song) {
-  //       return song.lyrics.toLowerCase().includes(query);
-  //     });
-  //   });
-
-  //   let filteredByLyrics = videos.filter(function(video) {
-  //     return video.songs.some(function(song) {
-  //       return song.lyrics.toLowerCase().includes(query);
-  //     });
-  //   });
-
-  //   this.setState({
-  //     allCount: videos.length,
-  //     bandCount: videos.filter( video => video.band.toLowerCase().includes(query) ).length,
-  //     songCount: filteredBySong.length,
-  //     lyricsCount: filteredByLyrics.length
-  //   })
-  // }
-
-  // filterResults = (videos, tab) => {
-  //   let query = this.props.location.pathname.split('/')[3].toLowerCase()
-  //   let filteredVideos = videos
-    
-  //   switch (tab) {
-  //     case "all":
-  //       this.props.getVideos(videos);
-  //       break;
-  //     case "band":
-  //       filteredVideos = videos.filter( video => video.band.toLowerCase().includes(query) )
-  //         this.props.getVideos(filteredVideos);
-  //         break;
-  //     case "songTitle":
-  //       filteredVideos = videos.filter(function(video) {
-  //         return video.songs.some(function(song) {
-  //           return song.title.toLowerCase().includes(query);
-  //         });
-  //       });
-  //       this.props.getVideos(filteredVideos);
-  //       break;
-  //     case "songLyrics":
-  //       filteredVideos = videos.filter(function(video) {
-  //         return video.songs.some(function(song) {
-  //           return song.lyrics.toLowerCase().includes(query);
-  //         });
-  //       });
-  //       this.props.getVideos(filteredVideos);
-  //       break;
-  //     default:
-  //       this.props.getVideos(filteredVideos);
-  //     }
-  
-  // }
-
-
-
-
-  // "/videos/search/:query"
-  render() {
-    // console.log(this.props,"======search results page========");
-    //TODO make a function accounts for 1 video
-    let query = this.props.location.pathname.split('/')[3]
-    
+  displayFilterTabs = () => {
     return (
-      <>
-    <VideoDashboard /> 
-   
-    
-      </>
+      <Nav fill variant="tabs" defaultActiveKey="all">
+        <Nav.Item>
+          <Nav.Link 
+            eventKey="all"
+            title="all"
+            onClick={() => this.handleTabClick("all") }
+          >
+              All({this.props.filteredByAll.length})
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link 
+            onClick={() => this.handleTabClick("bands") }
+            title="bands"
+            eventKey="band">Artist/Bands({this.props.filteredByBand.bands.length})</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link 
+            onClick={() => this.handleTabClick("songs")}
+            title="songs"
+            eventKey="songs">Songs({this.props.filteredBySong.songs.length})</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link 
+            onClick={() => this.handleTabClick("lyrics")}
+            title="lyrics"
+            eventKey="lyrics">Lyrics({this.props.filteredByLyrics.lyrics.length})</Nav.Link>
+        </Nav.Item>
+      </Nav> )
+  }
 
+  findVideos = () => {
+    switch (this.props.filter) {
+      case "none":
+        return this.props.videos
+      case "all":
+        return this.props.filteredByAll
+      case "bands":
+        return this.props.filteredByBand.bands 
+      case "songs":
+        return this.props.filteredBySong.songs
+      case "lyrics":
+        return this.props.filteredByLyrics.lyrics
+      default:
+        return this.props.videos
+    }
+  }
+
+  handleTabClick = (tab) => {
+    this.props.setFilter(tab)
+  }
+
+
+  render() {
+    const videosToDisplay = this.findVideos()
+    // console.log(this.props, "======VIDEO DASHBOARD=====");
+    return (
+      <Container fluid>
+      <div className="page-content-wrapper">
+        <div className="dashboard-header">
+          <div className="dashboard-header-title"><h1 className="header-text">Saicophonic Medicine</h1></div>
+          <h5 className="header-subtext">An expanding library of live music sessions</h5>
+        </div>
+        {this.props.showTabs === "true" ? this.displayFilterTabs() : null}
+        <VideoContainer videos={videosToDisplay} />
+      </div>
+      </Container>
     )
   }
 }
@@ -416,76 +126,30 @@ const setStateToProps = (state) => {
   return {
     videos: state.videos,
     filteredByAll: state.filteredByAll,
-    // videos: state.videos.videos,
-    // auth: state.auth
+    filteredByBand: state.filteredByBand,
+    filteredBySong: state.filteredBySong,
+    filteredByLyrics: state.filteredByLyrics,
+    filter: state.setFilter,
+    showTabs: state.toggleTabs.showTabs,
+    //note to self: naming the key setFilter here will not work because the props already have a key called setFilter pointing to the reducer
+    //setFilter: state.setFilter,
+    auth: state.auth
   };
 };
 
 const setDispatchToProps = {
-  // currentUser,
-  getVideos
+  currentUser,
+  getVideos,
+  setFilter,
+  toggleTabs
+  
 };
 
-export default connect(setStateToProps, setDispatchToProps)(SearchResults);
+export default connect(setStateToProps, setDispatchToProps)(VideoDashboard);
 
 
 
 
-
-
-//======================!SEARCH RESULTS=======================
-
-
-
-
-
-
-
-
-
-// else if (tab === "bands"){
-    //   let videos = this.props.filteredByBand.bands.map((video) => {
-    //     return (<VideoCard
-    //       {...video}
-    //       key={video.id}
-    //      />)
-    //     })
-    //     return videos
-    // }
-
-    // switch (tab) {
-    //   case "all":
-    //     let videos = this.props.filteredByAll.map((video) => {
-    //       return (<VideoCard
-    //         {...video}
-    //         key={video.id}
-    //        />)
-    //       })
-    //       return videos
-    //     break;
-    //   case "band":
-    //     videos = this.props.filteredByAll.filter( video => video.band.toLowerCase().includes(query) )
-    //       this.props.getVideos(filteredVideos);
-    //       break;
-    //   case "songTitle":
-    //     filteredVideos = this.props.filteredByAll.filter(function(video) {
-    //       return video.songs.some(function(song) {
-    //         return song.title.toLowerCase().includes(query);
-    //       });
-    //     });
-    //     this.props.getVideos(filteredVideos);
-    //     break;
-    //   case "songLyrics":
-    //     filteredVideos = this.props.filteredByAll.filter(function(video) {
-    //       return video.songs.some(function(song) {
-    //         return song.lyrics.toLowerCase().includes(query);
-    //       });
-    //     });
-    //     this.props.getVideos(filteredVideos);
-    //     break;
-    //   default:
-    //     this.props.getVideos(filteredVideos);
-    //   }
 
 
 
