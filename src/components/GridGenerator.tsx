66453d3dfdb12f76ed35ab8ac45325eb5ffd24fb
Natalia <n8galan@gmail.com>
2020-12-15 18:44:01 -0600

    renders grid dynamically

--- src/components/GridGenerator.tsx
+++ src/components/GridGenerator.tsx
@@ -1,25 +1,21 @@
 import chunk from 'lodash/chunk';
-
 import * as React from 'react';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-// const chunk = require('lodash/chunk')
-
 type GridGeneratorProps = {
-	cols: number;
+  cols: 1 | 2 | 3 | 4 | 6 | 12;
 };
-const GridGenerator: React.FC<GridGeneratorProps> = ({ cols: number, children }) => {
-	const colWidth = 12 / 4;
-  const rows = chunk(React.Children.toArray(children), 4);
+const GridGenerator: React.FC<GridGeneratorProps> = ({ cols, children }) => {
+	const colWidth = 12 / cols;
+  const rows = chunk(React.Children.toArray(children), cols);
 	return (
 		<>
 			{rows.map((cols: any, idx: number) => (
 				<Row key={idx} className='dashboard-row'>
 					{cols.map((col: any, idx: number) => (
 						// change to sm and md to show grid on regular ipad
-						// <Col sm={12} md={colWidth} key={idx}>
-						<Col md={12} lg={3} key={idx}>
+						<Col md={12} lg={colWidth} key={idx}>
 							{col}
 						</Col>
 					))}
